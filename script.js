55a50c7f5d116f4962c7c6578070d5349a551c69
nadezdamartynkevic <nadezda.martynkevic@gmail.com>
2023-01-12 22:16:05 +0000

    homework

---
+++ script.js
@@ -0,0 +1,49 @@
+
+// 1 task:
+
+
+// let message = (login == 'Сотрудник') ? 'Привет':
+// (login == 'Директор') ? 'Здравствуйте':
+// (login == '') ? 'Нет логина':
+// '';
+ 
+
+// 2 task: 
+
+    // const month = +prompt("Введите пожалуйста месяц:")
+
+    // if (month === 12 || month === 1 || month === 2) {
+    //     alert('зима')
+    // }
+    // if (month === 3 || month === 4 || month === 5) {
+    //     alert('весна')
+    // }
+    // if (month === 6 || month === 7 || month === 8) {
+    //     alert('лето')
+    // }
+    // if (month === 9 || month === 10 || month === 11) {
+    //     alert('осень')
+    // }
+
+
+
+
+
+
+    
+
+
+// 3 task
+
+// const n = 10;
+// for (let i = 2; i <= n; i++) {
+//   for (let j = 2; j <= i; j++) {
+//     if (i % j === 0 && j < i) {
+//       break;
+//     } else if (j === i) {
+//       console.log(i);
+//     }
+//   }
+// }
+
+
